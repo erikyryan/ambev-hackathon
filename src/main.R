adb79196c7d08e128676a91117be50be7ebb09c9
erikyryan <erikyryang@gmail.com>
2021-10-12 16:29:12 -0300

    adicionando conversao

--- src/main.R
+++ src/main.R
@@ -5,8 +5,16 @@ source("HWdata.r")
 source("MMdata.r")
 
 dados <- read.csv(file = "resources/data.csv")
-dados <- subset(data, select= -c(Longitude,Latitude))
+dados <- subset(dados, select= -c(Longitude,Latitude))
 
 colunas <- read.csv(file = "resources/data_column.csv")
 
+dias <- subset(dados, select = c(Doc..Date))
+
+for(i in 1:length(dias[,1])){
+  dados_data <- as.Date(dias[i,1]) #TODO: dados_data deve ser uma nova list, contendo os elementos de "dias", convertidos para o tipo Date 
+}
+
+
 HWdata(dados)
+typeof(dados[1,2])
