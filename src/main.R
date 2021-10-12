079c43902b90c480fec14db163af69a42f89a05c
erikyryan <erikyryang@gmail.com>
2021-10-11 23:05:25 -0300

    update files

--- src/main.R
+++ src/main.R
@@ -1,4 +1,30 @@
-library(forecast)
-library(fma)
-library(datasets)
-library(ggplot2)
+if(!require(gdata)) 
+install.packages("gdata");require(gdata)
+setwd("/home/...")
+dados <- read.csv("ambev-hackthon/resources/Data.xlsx - Data.csv")
+
+HoltWinters(x, alpha = NULL, beta = NULL, gamma = NULL,
+            seasonal = c("additive", "multiplicative"),
+            start.periods = 2, l.start = NULL, b.start = NULL,
+            s.start = NULL,
+            optim.start = c(alpha = 0.3, beta = 0.1, gamma = 0.1),
+            optim.control = list())
+
+require(graphics)
+
+## Seasonal Holt-Winters
+(m <- HoltWinters(co2))
+plot(m)
+plot(fitted(m))
+
+(m <- HoltWinters(AirPassengers, seasonal = "mult"))
+plot(m)
+
+## Non-Seasonal Holt-Winters
+x <- uspop + rnorm(uspop, sd = 5)
+m <- HoltWinters(x, gamma = FALSE)
+plot(m)
+
+## Exponential Smoothing
+m2 <- HoltWinters(x, gamma = FALSE, beta = FALSE)
+lines(fitted(m2)[,1], col = 3)
