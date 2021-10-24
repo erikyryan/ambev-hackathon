41d87df810d04d3e6e11dbad254384d19c202eef
Davi Fernandes Alves da Silva <daviifernandes06@gmail.com>
2021-10-24 12:05:05 -0300

    adicionar css

--- app/app.R
+++ app/app.R
@@ -2,8 +2,7 @@ if(!require(shiny)) install.packages("shiny");require(shiny)
 if(!require(shiny.router)) install.packages("shiny.router");require(shiny.router)
 
 
-principal <- htmlTemplate(filename = "../resources/index.html"
-
+principal <- htmlTemplate(filename = "../resources/index.html", document_ = "auto"
 )
 
 SobreNos <- htmlTemplate(filename = "../resources/aboutUs.html"
@@ -22,8 +21,8 @@ router <- make_router(
 
 
 ui <- fluidPage(
-  theme = "../resources/css/style.css", #importar tema CSS
-  
+  includeCSS("../resources/css/style.css"),
+
   tags$div(class="navbar navbar-expand-md",
            tags$div(class="container",
                     tags$a(href = route_link("/"),class="navbar-brand"),
@@ -41,36 +40,7 @@ ui <- fluidPage(
                     )
               )
       ),
-    router$ui
-  
-  # tags$div(class="collapse navbar-collapse",id="navbarNav",
-  #          tags$ul( class ="navbar-nav ml-auto",
-  #                   tags$li( class="nav-item active",
-  #                            tags$a()
-  #                            )
-  #                   )
-  #   )
-  
-  
-  # <div class="navbar navbar-expand-md"> 
-  #   <div class="container">
-  #   <a href="index.html" class="navbar-brand"></a>
-  #   <!--<img alt="" align="left" src="img/logo.png" height="100" width="30"> --> 
-  #   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-  #   <i class="fas fa-align-justify"></i>
-  #   </button>
-  #   <div class="collapse navbar-collapse" id="navbarNav">
-  #   <ul class="navbar-nav ml-auto">
-  #   <li class="nav-item active">
-  #   <a class="nav-link" href="index.html">Página Inicial <span class="sr-only">(current)</span></a>
-  #   </li>
-  #   <li class="nav-item">
-  #   <a class="nav-link" href="aboutUs.html">Sobre nós</a>
-  #   </li>
-  #   </ul>
-  #   </div>
-  #   </div>
-  #   </div>
+    router$ui,
 )
 
 
