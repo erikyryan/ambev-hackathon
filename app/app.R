290dabecc2921de486c96c6cba39e3acefb9a3e5
Davi Fernandes Alves da Silva <daviifernandes06@gmail.com>
2021-10-25 11:16:52 -0300

    alterações no front-end esquema de inclusões

--- app/app.R
+++ app/app.R
@@ -1,32 +1,19 @@
 if(!require(shiny)) install.packages("shiny");require(shiny)
 if(!require(shiny.router)) install.packages("shiny.router");require(shiny.router)
 
-style <- includeCSS("../resources/css/style.css")
-fontawesome <- includeCSS("../resources/css/fontawesome.min.css")
-animate <- includeCSS("../resources/css/animate.css")
-bootstrap_drop <- includeCSS("../resources/css/bootstrap-dropdownhover.min.css")
-bootstrap <- includeCSS("../resources/css/bootstrap.min.css")
-aos <- includeCSS("../resources/css/aos.css")
-
-
-bootstrapjs <- includeScript("../resources/js/bootstrap.min.js")
-fontawesomejs <- tags$script(src="../resources/js/fontawesome.js") 
-bootstrapdropjs <- includeScript("../resources/js/bootstrap-dropdownhover.min.js") 
-aosjs <- includeScript("../resources/js/aos.js")
-customjs <- includeScript("../resources/js/custom.js")
 
 principal <- htmlTemplate(filename = "../resources/index.html", document_ = "auto", 
-                          style = style,
-                          fontawesome = fontawesome,
-                          bootstrap_min = bootstrap,
-                          animate= animate,
-                          bootstrap_dropdownhover = bootstrap_drop,
-                          aos = aos,
-                          bootstrap_js =  bootstrapjs,
-                          fontawesome_js  = fontawesomejs,
-                          bootstrapdrop_js = bootstrapdropjs,
-                          aos_js = aosjs,
-                          custom_js = customjs
+                          style = includeCSS("../resources/css/style.css"),
+                          fontawesome = includeCSS("../resources/css/fontawesome.min.css"),
+                          bootstrap_min = includeCSS("../resources/css/bootstrap.min.css"),
+                          animate = includeCSS("../resources/css/animate.css"),
+                          bootstrap_dropdownhover = includeCSS("../resources/css/bootstrap-dropdownhover.min.css"),
+                          aos = includeCSS("../resources/css/aos.css"),
+                          bootstrap_js =  includeScript("../resources/js/bootstrap.min.js"),
+                          fontawesome_js  = tags$script(src="../resources/js/fontawesome.js"),
+                          bootstrapdrop_js = includeScript("../resources/js/bootstrap-dropdownhover.min.js"),
+                          aos_js =  includeScript("../resources/js/aos.js"),
+                          custom_js = includeScript("../resources/js/custom.js")
 )
 
 SobreNos <- htmlTemplate(filename = "../resources/aboutUs.html", document_ = "auto"
